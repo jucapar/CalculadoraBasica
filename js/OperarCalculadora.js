a93070dbf334fc87eb10908d0e4ad2346ab425fa
jucapar <juanpastorcarlos@gmail.com>
2017-10-20 12:20:47 +0200

    Calculadora Basica 20/11/2017

--- js/OperarCalculadora.js
+++ js/OperarCalculadora.js
@@ -4,5 +4,20 @@
 	Fecha de creacion: 20/10/2017
 	Fecha de modificacion:20/10/2017
 	
-	
+	Funciones creadas:
+	mostrarVisor(valor): Escribe el numero del boton pulsado en el campo del visor
 */
+
+	var valorVisor = '';
+	function mostrarVisor(valor){
+		
+		valorVisor += valor;
+		document.getElementById("visor").value = valorVisor;
+	}
+
+	function borrarVisor(){
+		document.getElementById("visor").value = "0";
+		valorVisor="";
+	}
+
+
